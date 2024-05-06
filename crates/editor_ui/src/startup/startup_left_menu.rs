4c399e953d2c7ee96b2a597e5a56ade0461497cc
niklas <n.hack2810@gmail.com>
2024-05-06 14:47:47 +0200

    ui

--- crates/editor_ui/src/startup/startup_left_menu.rs
+++ crates/editor_ui/src/startup/startup_left_menu.rs
@@ -5,11 +5,34 @@ pub(crate) fn startup_left_menu(builder: &mut ChildBuilder) {
         style: Style {
             width: Val::Percent(30.0),
             flex_direction: FlexDirection::Column,
+            justify_content: JustifyContent::Center,
             ..default()
         },
-        background_color: BackgroundColor(Color::BLUE),
         ..default()
-    }).with_children(|_parent| {
+    }).with_children(|parent| {
+        startup_left_menu_entry(parent, "Projects");
+    });
+}
 
+#[derive(Component)]
+struct StartupLeftMenuEntry {
+    pub name: String,
+}
+
+fn startup_left_menu_entry(builder: &mut ChildBuilder, title: &str) {
+    builder.spawn((NodeBundle {
+        style: Style {
+            padding: UiRect::axes(Val::Percent(5.0), Val::Percent(4.0)),
+            justify_content: JustifyContent::FlexEnd,
+            ..default()
+        },
+        ..default()
+    }, StartupLeftMenuEntry {
+        name: title.to_string(),
+    })).with_children(|parent| {
+        parent.spawn(TextBundle {
+            text: Text::from_section(title, TextStyle::default()),
+            ..default()
+        });
     });
 }
