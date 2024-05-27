fa152af55be95b78038e349cc3f8599e8635bf04
Cr4shd3v <n.hack2810@gmail.com>
2024-05-27 18:27:42 +0200

    close #7

--- crates/editor_ui/src/editor/main_editor_screen.rs
+++ crates/editor_ui/src/editor/main_editor_screen.rs
@@ -8,13 +8,63 @@ pub(super) fn spawn_main_editor_screen(
 ) {
     for (ui_root, project_window) in window_query.iter() {
         commands.entity(ui_root.root).despawn_descendants().with_children(|parent| {
-            parent.spawn(TextBundle::from_section(
-                format!("Editor at {}", project_window.project_editor_config.path),
-                TextStyle {
-                    font: DefaultFonts::ROBOTO_REGULAR,
+            parent.spawn(NodeBundle {
+                style: Style {
+                    height: Val::Vh(4.0),
+                    width: Val::Vw(100.0),
+                    flex_direction: FlexDirection::Row,
                     ..default()
                 },
-            ));
+                background_color: BackgroundColor(Color::RED),
+                ..default()
+            });
+
+            parent.spawn(NodeBundle {
+                style: Style {
+                    height: Val::Vh(76.0),
+                    width: Val::Vw(100.0),
+                    flex_direction: FlexDirection::Row,
+                    ..default()
+                },
+                ..default()
+            }).with_children(|parent| {
+                parent.spawn(NodeBundle {
+                    style: Style {
+                        height: Val::Percent(100.0),
+                        width: Val::Percent(20.0),
+                        ..default()
+                    },
+                    background_color: BackgroundColor(Color::GREEN),
+                    ..default()
+                });
+                parent.spawn(NodeBundle {
+                    style: Style {
+                        height: Val::Percent(100.0),
+                        width: Val::Percent(80.0),
+                        ..default()
+                    },
+                    background_color: BackgroundColor(Color::GOLD),
+                    ..default()
+                }).with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        format!("Editor at {}", project_window.project_editor_config.path),
+                        TextStyle {
+                            font: DefaultFonts::ROBOTO_REGULAR,
+                            ..default()
+                        },
+                    ));
+                });
+            });
+
+            parent.spawn(NodeBundle {
+                style: Style {
+                    height: Val::Vh(20.0),
+                    width: Val::Vw(100.0),
+                    ..default()
+                },
+                background_color: BackgroundColor(Color::BLUE),
+                ..default()
+            });
         });
     }
 }
