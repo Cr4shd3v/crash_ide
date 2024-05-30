d7373dd0a29f30ad00db3b218d33466a7b99aa9c
Cr4shd3v <n.hack2810@gmail.com>
2024-05-30 19:03:21 +0200

    progress #14

--- crates/editor_ui/src/editor/editor_left_menu/filesystem_menu.rs
+++ crates/editor_ui/src/editor/editor_left_menu/filesystem_menu.rs
@@ -15,19 +15,23 @@ impl Plugin for FilesystemMenuPlugin {
 }
 
 #[derive(Component)]
-pub struct ProjectRoot;
+pub struct ProjectRoot {
+    base_path: String,
+}
 
 #[derive(Component)]
 pub struct FileDisplay {
     filename: String,
     is_file: bool,
+    level: u16,
 }
 
 impl FileDisplay {
-    pub fn new(filename: String, is_file: bool) -> Self {
+    pub fn new(filename: String, is_file: bool, level: u16) -> Self {
         Self {
             filename,
             is_file,
+            level,
         }
     }
 }
@@ -52,81 +56,91 @@ fn spawn_left_menu(
 
         commands.entity(entity).despawn_descendants().with_children(|parent| {
             parent.spawn((
-                FileDisplay::new(project.editor_project.name.clone(), false),
-                ProjectRoot,
-            )).with_children(|parent| {
-                parent.spawn(TextBundle {
-                    text: Text::from_section(base_path, TextStyle {
-                        font: DefaultFonts::ROBOTO_REGULAR,
-                        font_size: 14.0,
-                        color: Color::GRAY.with_a(0.5),
-                    }),
-                    style: Style {
-                        margin: UiRect::left(Val::Px(2.0)),
-                        ..default()
-                    },
-                    ..default()
-                });
-            });
+                FileDisplay::new(project.editor_project.name.clone(), false, 0),
+                ProjectRoot { base_path: base_path.clone() },
+            ));
         });
     }
 }
 
 fn spawn_all_rows(
     mut commands: Commands,
-    query: Query<(Entity, &FileDisplay), Added<FileDisplay>>,
+    query: Query<(Entity, &FileDisplay, Option<&ProjectRoot>), Added<FileDisplay>>,
     icons: Res<DefaultIcons>,
 ) {
-    for (entity, file_display) in query.iter() {
-        let mut childs = vec![];
-        childs.push(commands.spawn(ImageBundle {
-            image: UiImage {
-                texture: icons.right.clone(),
-                ..default()
-            },
-            style: Style {
-                height: Val::Px(22.5),
-                ..default()
-            },
-            ..default()
-        }).id());
-
-        childs.push(commands.spawn(ImageBundle {
-            image: UiImage {
-                texture: if file_display.is_file {
-                    icons.unknown_file.clone()
-                } else {
-                    icons.folder.clone()
-                },
-                ..default()
-            },
-            style: Style {
-                height: Val::Px(22.5),
-                ..default()
-            },
-            ..default()
-        }).id());
-
-        childs.push(commands.spawn(TextBundle {
-            text: Text::from_section(file_display.filename.clone(), TextStyle {
-                font: DefaultFonts::ROBOTO_REGULAR,
-                font_size: 14.0,
-                ..default()
-            }),
-            style: Style {
-                margin: UiRect::horizontal(Val::Px(4.0)),
-                ..default()
-            },
-            ..default()
-        }).id());
-
+    for (entity, file_display, root) in query.iter() {
         commands.entity(entity).insert(NodeBundle {
             style: Style {
-                flex_direction: FlexDirection::Row,
-                align_items: AlignItems::Center,
+                flex_direction: FlexDirection::Column,
                 ..default()
             },
             ..default()
-        }).insert_children(0, childs.as_slice());
+        }).with_children(|parent| {
+            parent.spawn(NodeBundle {
+                style: Style {
+                    flex_direction: FlexDirection::Row,
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                ..default()
+            }).with_children(|parent| {
+                parent.spawn(ImageBundle {
+                    image: UiImage {
+                        texture: icons.right.clone(),
+                        ..default()
+                    },
+                    style: Style {
+                        height: Val::Px(22.5),
+                        margin: UiRect::left(Val::Px(8.0 * file_display.level as f32)),
+                        ..default()
+                    },
+                    ..default()
+                });
+
+                parent.spawn(ImageBundle {
+                    image: UiImage {
+                        texture: if file_display.is_file {
+                            icons.unknown_file.clone()
+                        } else {
+                            icons.folder.clone()
+                        },
+                        ..default()
+                    },
+                    style: Style {
+                        height: Val::Px(22.5),
+                        ..default()
+                    },
+                    ..default()
+                });
+
+                parent.spawn(TextBundle {
+                    text: Text::from_section(file_display.filename.clone(), TextStyle {
+                        font: DefaultFonts::ROBOTO_REGULAR,
+                        font_size: 14.0,
+                        ..default()
+                    }),
+                    style: Style {
+                        margin: UiRect::horizontal(Val::Px(4.0)),
+                        ..default()
+                    },
+                    ..default()
+                });
+
+                if let Some(root) = root {
+                    parent.spawn(TextBundle {
+                        text: Text::from_section(root.base_path.clone(), TextStyle {
+                            font: DefaultFonts::ROBOTO_REGULAR,
+                            font_size: 14.0,
+                            color: Color::GRAY.with_a(0.5),
+                        }),
+                        style: Style {
+                            margin: UiRect::left(Val::Px(2.0)),
+                            ..default()
+                        },
+                        ..default()
+                    });
+                }
+            });
+        });
     }
 }
