eeecee272fa913949e3cc34812383167e1817b78
Cr4shd3v <n.hack2810@gmail.com>
2024-06-05 20:35:06 +0200

    progress #12

--- crates/editor_ui/src/editor/editor_bottom_menu/console_menu.rs
+++ crates/editor_ui/src/editor/editor_bottom_menu/console_menu.rs
@@ -1,10 +1,13 @@
-use std::io::{Read, Write};
+use std::io::Read;
+use std::path::PathBuf;
 use std::process::{Child, Command, Stdio};
 use std::sync::{Arc, Mutex};
-use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::mpsc::{channel, Receiver};
 use bevy::prelude::*;
 use bevy::tasks::{AsyncComputeTaskPool, Task};
 use editor_assets::DefaultFonts;
+use editor_config::FindProjectInParents;
+use editor_widget::{TextInputBundle, TextInputSettings, TextInputTextStyle};
 use crate::editor::main_editor_screen::EditorBottomMenu;
 
 pub(super) struct ConsoleMenuPlugin;
@@ -12,35 +15,108 @@ pub(super) struct ConsoleMenuPlugin;
 impl Plugin for ConsoleMenuPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_systems(Update, (spawn_console_menu, console_stdout))
+            .add_systems(Update, (spawn_console_instance, spawn_console_progress, console_stdout))
         ;
     }
 }
 
 #[derive(Component)]
-struct Console {
+struct ConsoleInstance {
+    directory: PathBuf,
+}
+
+fn spawn_console_instance(
+    mut commands: Commands,
+    query: Query<Entity, Added<EditorBottomMenu>>,
+    find_project_in_parents: FindProjectInParents,
+) {
+    for entity in query.iter() {
+        let project = find_project_in_parents.find(entity);
+
+        commands.entity(entity).despawn_descendants().with_children(|parent| {
+            parent.spawn((NodeBundle {
+                style: Style {
+                    flex_direction: FlexDirection::Column,
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    ..default()
+                },
+                ..default()
+            }, ConsoleInstance {
+                directory: PathBuf::from(&project.editor_project.path),
+            })).with_children(|parent| {
+                parent.spawn(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Row,
+                        ..default()
+                    },
+                    ..default()
+                }).with_children(|parent| {
+                    parent.spawn(TextBundle {
+                        text: Text::from_section(format!("{}$", project.editor_project.path), TextStyle {
+                            font: DefaultFonts::JETBRAINS_MONO_REGULAR,
+                            font_size: 18.0,
+                            ..default()
+                        }),
+                        style: Style {
+                            margin: UiRect::vertical(Val::Px(5.0)),
+                            ..default()
+                        },
+                        ..default()
+                    });
+
+                    parent.spawn((NodeBundle {
+                        style: Style {
+                            flex_grow: 1.0,
+                            ..default()
+                        },
+                        ..default()
+                    }, TextInputBundle {
+                        text_input_text_style: TextInputTextStyle::default().with_font(DefaultFonts::JETBRAINS_MONO_REGULAR),
+                        text_input_settings: TextInputSettings {
+                            with_border: false,
+                            ..default()
+                        },
+                        ..default()
+                    }));
+                });
+            });
+        });
+    }
+}
+
+#[derive(Component)]
+struct ConsoleCommand {
+    command: String,
+    directory: PathBuf,
+}
+
+#[derive(Component)]
+struct ConsoleProgress {
     #[allow(unused)]
     cmd: Child,
-    pub stdin_write: Sender<Vec<u8>>,
     pub stdout_read: Arc<Mutex<Receiver<Vec<u8>>>>,
-    stdin_task: Task<()>,
     stdout_task: Task<()>,
     stderr_task: Task<()>,
 }
 
-fn spawn_console_menu(
+fn spawn_console_progress(
     mut commands: Commands,
-    query: Query<Entity, Added<EditorBottomMenu>>,
+    query: Query<(Entity, &ConsoleCommand), Added<ConsoleCommand>>,
 ) {
-    for entity in query.iter() {
-        #[cfg(target_os = "linux")]
-        let mut cmd = Command::new("rev");
+    for (entity, command) in query.iter() {
         #[cfg(target_os = "windows")]
-        let mut cmd = Command::new("cmd");
-        let cmd = cmd
-            .stdin(Stdio::piped())
+        let cmd = Command::new("cmd")
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
+            .spawn();
+        #[cfg(target_os = "linux")]
+        let cmd = Command::new("bash")
+            .arg("-c")
+            .arg(&command.command)
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .current_dir(&command.directory)
             .spawn();
 
         if let Err(e) = cmd {
@@ -56,20 +132,12 @@ fn spawn_console_menu(
 
         let mut cmd = cmd.unwrap();
 
-        let mut stdin = cmd.stdin.take().unwrap();
         let mut stdout = cmd.stdout.take().unwrap();
         let mut stderr = cmd.stderr.take().unwrap();
 
-        let (stdin_write, stdin_read) = channel::<Vec<u8>>();
         let (stdout_write, stdout_read) = channel();
 
         let pool = AsyncComputeTaskPool::get();
-
-        let stdin_task = pool.spawn(async move {
-            while let Ok(bytes) = stdin_read.recv() {
-                stdin.write_all(bytes.as_slice()).unwrap();
-            }
-        });
 
         let cloned_write = stdout_write.clone();
         let stdout_task = pool.spawn(async move {
@@ -95,14 +163,12 @@ fn spawn_console_menu(
         });
 
         commands.entity(entity).despawn_descendants().with_children(|parent| {
-            parent.spawn((Console {
+            parent.spawn((ConsoleProgress {
                 cmd,
-                stdin_write,
                 stdout_read: Arc::new(Mutex::new(stdout_read)),
-                stdin_task,
                 stdout_task,
                 stderr_task,
-            }, TextBundle::from_section("", TextStyle {
+            }, TextBundle::from_section(command.command.clone(), TextStyle {
                 font: DefaultFonts::ROBOTO_REGULAR,
                 ..default()
             })));
@@ -111,7 +177,7 @@ fn spawn_console_menu(
 }
 
 fn console_stdout(
-    mut query: Query<(&mut Text, &Console)>
+    mut query: Query<(&mut Text, &ConsoleProgress)>
 ) {
     for (mut text, console) in query.iter_mut() {
         let mut result = vec![];
@@ -121,8 +187,6 @@ fn console_stdout(
 
         if !result.is_empty() {
             text.sections[0].value.push_str(&*String::from_utf8(result).unwrap());
-        } else {
-            console.stdin_write.send("echo hello".as_bytes().to_vec()).unwrap();
         }
     }
 }
