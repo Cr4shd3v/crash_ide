e317a28f1e9a936a31e6a76cd56592a45e5611fe
Cr4shd3v <n.hack2810@gmail.com>
2024-06-17 22:56:14 +0200

    added docs

--- crates/crash_ide_clipboard/src/lib.rs
+++ crates/crash_ide_clipboard/src/lib.rs
@@ -1,8 +1,13 @@
+#![warn(missing_docs)]
+//! This crate implements the arboard Clipboard as bevy resource.
+
+
 use std::borrow::Cow;
 use std::sync::{Arc, Mutex};
 use arboard::Error;
 use bevy::prelude::*;
 
+/// Plugin adding the clipboard.
 pub struct CrashIDEClipboardPlugin;
 
 impl Plugin for CrashIDEClipboardPlugin {
@@ -13,16 +18,19 @@ impl Plugin for CrashIDEClipboardPlugin {
     }
 }
 
+/// Wrapper around the [arboard Clipboard](arboard::Clipboard) as bevy resource
 #[derive(Resource)]
 pub struct Clipboard {
     inner: Arc<Mutex<arboard::Clipboard>>,
 }
 
 impl Clipboard {
+    /// See [get_text](arboard::Clipboard::get_text)
     pub fn get_text(&self) -> Result<String, Error> {
         self.inner.lock().unwrap().get_text()
     }
 
+    /// See [get_text](arboard::Clipboard::set_text)
     pub fn set_text<'a, T: Into<Cow<'a, str>>>(&self, text: T) -> Result<(), Error> {
         self.inner.lock().unwrap().set_text(text)
     }
