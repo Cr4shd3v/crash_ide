9cdff902d8e114e09c918300626dcc358b200a3f
Cr4shd3v <n.hack2810@gmail.com>
2024-06-19 22:36:14 +0200

    progress #81, #79

--- crates/crash_ide_ui/src/settings/general_settings_menu.rs
+++ crates/crash_ide_ui/src/settings/general_settings_menu.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 use crash_ide_assets::DefaultFonts;
+use crash_ide_config::GeneralSettings;
+use crash_ide_widget::{Checkbox, CheckboxBundle, CheckboxLabel};
 
 pub(super) struct GeneralSettingsMenuPlugin;
 
@@ -17,17 +19,21 @@ pub struct GeneralSettingsMenu;
 fn spawn_general_settings_menu(
     mut commands: Commands,
     query: Query<Entity, Added<GeneralSettingsMenu>>,
+    settings: Res<GeneralSettings>,
 ) {
     for entity in query.iter() {
         commands.entity(entity).despawn_descendants().with_children(|parent| {
-            parent.spawn(TextBundle {
-                text: Text::from_section("test", TextStyle {
-                    font: DefaultFonts::ROBOTO_REGULAR,
-                    font_size: 18.0,
+            parent.spawn((
+                CheckboxBundle {
+                    checkbox: Checkbox::from_bool(settings.open_last_project_on_startup),
+                    label: CheckboxLabel::End(TextSection::new("Open last opened project on startup", TextStyle {
+                        font: DefaultFonts::ROBOTO_REGULAR,
+                        font_size: 18.0,
+                        ..default()
+                    })),
                     ..default()
-                }),
-                ..default()
-            });
+                }
+            ));
         });
     }
 }
--- crates/crash_ide_widget/src/input/checkbox.rs
+++ crates/crash_ide_widget/src/input/checkbox.rs
@@ -21,6 +21,10 @@ pub struct CheckboxBundle {
     pub checkbox: Checkbox,
     /// Label of the checkbox
     pub label: CheckboxLabel,
+    /// Styling of the checkbox
+    pub checkbox_style: CheckboxStyle,
+    /// Interaction to detect the click
+    pub interaction: Interaction,
 }
 
 /// Represents the value of a checkbox.
@@ -33,6 +37,22 @@ pub enum Checkbox {
     Unchecked,
 }
 
+impl Checkbox {
+    /// Whether self is [Checkbox::Checked]
+    pub fn is_checked(&self) -> bool {
+        matches!(self, Checkbox::Checked)
+    }
+
+    /// Creates a [Checkbox] from a bool
+    pub fn from_bool(value: bool) -> Self {
+        if value {
+            Checkbox::Checked
+        } else {
+            Checkbox::Unchecked
+        }
+    }
+}
+
 /// Contains the label definition.
 #[derive(Component, Default)]
 pub enum CheckboxLabel {
@@ -40,15 +60,51 @@ pub enum CheckboxLabel {
     #[default]
     None,
     /// Label will be created right to the checkbox
-    Right(TextSection),
+    End(TextSection),
     /// Label will be created left to the checkbox
-    Left(TextSection),
+    Start(TextSection),
+}
+
+/// Styling for a checkbox
+#[derive(Component)]
+pub struct CheckboxStyle {
+    /// Color when the checkbox is checked
+    pub active_color: Color,
+}
+
+impl Default for CheckboxStyle {
+    fn default() -> Self {
+        Self {
+            active_color: Color::GRAY.with_a(0.1),
+        }
+    }
 }
 
 fn create_checkbox(
-    query: Query<(Entity, &CheckboxLabel), Added<Checkbox>>,
+    mut commands: Commands,
+    query: Query<(Entity, &CheckboxLabel, &Checkbox, &CheckboxStyle), Added<Checkbox>>,
 ) {
-    for (_entity, _label) in query.iter() {
+    for (entity, label, value, checkbox_style) in query.iter() {
+        commands.entity(entity).with_children(|parent| {
+            if let CheckboxLabel::Start(section) = label {
+                parent.spawn(TextBundle::from_sections(vec![section.clone()]));
+            }
 
+            parent.spawn(NodeBundle {
+                style: Style {
+                    width: Val::Px(12.0),
+                    height: Val::Px(12.0),
+                    border: UiRect::all(Val::Px(1.0)),
+                    ..default()
+                },
+                border_color: BorderColor(Color::GRAY.with_a(0.1)),
+                background_color: BackgroundColor(if value.is_checked() { checkbox_style.active_color } else { Color::NONE }),
+                ..default()
+            });
+
+            if let CheckboxLabel::End(section) = label {
+                parent.spawn(TextBundle::from_sections(vec![section.clone()]));
+            }
+        });
     }
 }
