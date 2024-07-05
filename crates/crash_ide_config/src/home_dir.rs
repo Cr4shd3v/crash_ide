5a53435768dcd6e408e358578fd9bec92ed7a5f0
niklas <n.hack2810@gmail.com>
2024-07-05 21:46:29 +0200

    migrate home to 0.3

--- crates/crash_ide_config/src/home_dir.rs
+++ crates/crash_ide_config/src/home_dir.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::PathBuf;
 use bevy::prelude::{Commands, Resource};
-use homedir::get_my_home;
+use homedir::my_home;
 
 /// Resource containing the config and project path
 #[derive(Resource)]
@@ -15,7 +15,7 @@ pub struct HomeDir {
 }
 
 pub(crate) fn load_home_dir(mut commands: Commands) {
-    let home_path = get_my_home().expect("Could not determine home directory").unwrap();
+    let home_path = my_home().expect("Could not determine home directory").unwrap();
     let base_config_path = home_path.join(".crash_ide");
     if fs::metadata(&base_config_path).is_err() {
         fs::create_dir(&base_config_path).unwrap();
