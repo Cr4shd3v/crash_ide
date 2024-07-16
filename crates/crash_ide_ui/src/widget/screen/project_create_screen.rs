bb417b4e20050897f98d0c0785f4f737dc24f5fa
Cr4shd3v <n.hack2810@gmail.com>
2024-07-16 20:59:40 +0200

    progress #27

--- crates/crash_ide_ui/src/widget/screen/project_create_screen.rs
+++ crates/crash_ide_ui/src/widget/screen/project_create_screen.rs
@@ -1,14 +1,15 @@
 use std::fs;
 use std::path::PathBuf;
 use std::str::FromStr;
+
 use bevy::color::palettes::css::RED;
 use bevy::prelude::*;
 
+use crash_ide_assets::{DefaultColors, DefaultFonts};
 use crash_ide_config::{EditorConfigProjects, HomeDir};
+use crash_ide_project::{EditorProject, OpenProjectEvent, ProjectFiles};
 use crash_ide_widget::{TextInputBundle, TextInputSettings, TextInputTextStyle, TextInputValue};
 
-use crash_ide_assets::{DefaultColors, DefaultFonts};
-use crash_ide_project::{EditorProject, OpenProjectEvent, ProjectFiles};
 use crate::trigger::Clicked;
 use crate::widget::folder_input::FolderInput;
 use crate::window::AllWindows;
@@ -50,80 +51,111 @@ fn spawn_project_create_screen(
         commands.entity(all_windows.get(&window_ui_root).ui_root).despawn_descendants().with_children(|parent| {
             parent.spawn(NodeBundle {
                 style: Style {
-                    flex_direction: FlexDirection::Column,
-                    padding: UiRect::axes(Val::Percent(10.0), Val::Percent(5.0)),
+                    flex_direction: FlexDirection::Row,
+                    height: Val::Percent(100.0),
                     ..default()
                 },
                 ..default()
             }).with_children(|parent| {
-                parent.spawn(TextBundle::from_section("Project Path", TextStyle {
-                    font: DefaultFonts::ROBOTO_REGULAR,
-                    font_size: 18.0,
-                    ..default()
-                }));
-
-                parent.spawn((
-                    TextInputBundle {
-                        text_input_value: TextInputValue(home_dir.projects_path.join(DEFAULT_NEW_PROJECT_NAME).to_str().unwrap().to_string()),
-                        text_input_text_style: TextInputTextStyle::default().with_font(DefaultFonts::ROBOTO_REGULAR),
-                        text_input_settings: TextInputSettings {
-                            input_width: Val::Percent(96.0),
-                            ..default()
-                        },
-                        ..default()
-                    },
-                    NodeBundle {
-                        style: Style {
-                            width: Val::Vw(80.0),
-                            justify_content: JustifyContent::SpaceBetween,
-                            ..default()
-                        },
-                        ..default()
-                    },
-                    ProjectPathInput,
-                    FolderInput,
-                ));
-
-                parent.spawn((TextBundle {
-                    text: Text::from_section("", TextStyle {
-                        font: DefaultFonts::ROBOTO_REGULAR,
-                        color: RED.into(),
-                        font_size: 18.0,
-                    }),
+                parent.spawn(NodeBundle {
                     style: Style {
-                        margin: UiRect::vertical(Val::Px(3.0)),
+                        flex_direction: FlexDirection::Column,
+                        width: Val::Percent(20.0),
+                        border: UiRect::right(Val::Px(2.0)),
                         ..default()
                     },
+                    border_color: BorderColor(DefaultColors::GRAY.with_alpha(0.2)),
                     ..default()
-                }, CreateProjectPathErrorText));
+                });
 
                 parent.spawn(NodeBundle {
                     style: Style {
-                        flex_direction: FlexDirection::RowReverse,
+                        flex_direction: FlexDirection::Column,
+                        padding: UiRect::axes(Val::Percent(5.0), Val::Percent(2.5)),
+                        width: Val::Percent(80.0),
                         ..default()
                     },
                     ..default()
                 }).with_children(|parent| {
-                    parent.spawn((
-                        NodeBundle {
-                            style: Style {
-                                padding: UiRect::axes(Val::Px(5.0), Val::Px(2.5)),
-                                margin: UiRect::top(Val::Vh(2.5)),
-                                ..default()
-                            },
-                            background_color: BackgroundColor(DefaultColors::PRIMARY_BUTTON),
-                            border_radius: BorderRadius::all(Val::Px(2.0)),
+                    parent.spawn(TextBundle::from_section("Project Path", TextStyle {
+                        font: DefaultFonts::ROBOTO_REGULAR,
+                        font_size: 18.0,
+                        ..default()
+                    }));
+
+                    parent.spawn(NodeBundle {
+                        style: Style {
+                            flex_direction: FlexDirection::Row,
+                            justify_content: JustifyContent::Center,
+                            width: Val::Percent(100.0),
                             ..default()
                         },
-                        Interaction::None,
-                        Button,
-                        CreateProjectConfirmButton,
-                    )).observe(create_project_confirm).with_children(|parent| {
-                        parent.spawn(TextBundle::from_section("Create", TextStyle {
-                            font_size: 18.0,
+                        ..default()
+                    }).with_children(|parent| {
+                        parent.spawn((
+                            TextInputBundle {
+                                text_input_value: TextInputValue(home_dir.projects_path.join(DEFAULT_NEW_PROJECT_NAME).to_str().unwrap().to_string()),
+                                text_input_text_style: TextInputTextStyle::default().with_font(DefaultFonts::ROBOTO_REGULAR),
+                                text_input_settings: TextInputSettings {
+                                    input_width: Val::Percent(96.0),
+                                    ..default()
+                                },
+                                ..default()
+                            },
+                            NodeBundle {
+                                style: Style {
+                                    width: Val::Percent(100.0),
+                                    justify_content: JustifyContent::SpaceBetween,
+                                    ..default()
+                                },
+                                ..default()
+                            },
+                            ProjectPathInput,
+                            FolderInput,
+                        ));
+                    });
+
+                    parent.spawn((TextBundle {
+                        text: Text::from_section("", TextStyle {
                             font: DefaultFonts::ROBOTO_REGULAR,
+                            color: RED.into(),
+                            font_size: 18.0,
+                        }),
+                        style: Style {
+                            margin: UiRect::vertical(Val::Px(3.0)),
                             ..default()
-                        }));
+                        },
+                        ..default()
+                    }, CreateProjectPathErrorText));
+
+                    parent.spawn(NodeBundle {
+                        style: Style {
+                            flex_direction: FlexDirection::RowReverse,
+                            ..default()
+                        },
+                        ..default()
+                    }).with_children(|parent| {
+                        parent.spawn((
+                            NodeBundle {
+                                style: Style {
+                                    padding: UiRect::axes(Val::Px(5.0), Val::Px(2.5)),
+                                    margin: UiRect::top(Val::Vh(2.5)),
+                                    ..default()
+                                },
+                                background_color: BackgroundColor(DefaultColors::PRIMARY_BUTTON),
+                                border_radius: BorderRadius::all(Val::Px(2.0)),
+                                ..default()
+                            },
+                            Interaction::None,
+                            Button,
+                            CreateProjectConfirmButton,
+                        )).observe(create_project_confirm).with_children(|parent| {
+                            parent.spawn(TextBundle::from_section("Create", TextStyle {
+                                font_size: 18.0,
+                                font: DefaultFonts::ROBOTO_REGULAR,
+                                ..default()
+                            }));
+                        });
                     });
                 });
             });
